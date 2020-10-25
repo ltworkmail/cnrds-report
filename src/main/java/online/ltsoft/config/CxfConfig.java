52abffbc20f314bb690ab7ece187b8aac3c1531e
lintao <ltworkmail@163.com>
2020-10-25 23:59:57 +0800

    cnrds-report项目代码调整

--- src/main/java/online/ltsoft/config/CxfConfig.java
+++ src/main/java/online/ltsoft/config/CxfConfig.java
@@ -2,8 +2,11 @@ package online.ltsoft.config;
 
 import online.ltsoft.common.cxf.ApiService;
 import org.apache.cxf.Bus;
+import org.apache.cxf.bus.spring.SpringBus;
 import org.apache.cxf.jaxws.EndpointImpl;
+import org.apache.cxf.transport.servlet.CXFServlet;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import javax.xml.ws.Endpoint;
@@ -11,9 +14,14 @@ import javax.xml.ws.Endpoint;
 @Configuration
 public class CxfConfig {
     @Autowired
-    private Bus bus;
+    Bus bus;
     @Autowired
     private ApiService apiService;
+
+    @Bean("cxfServletRegistration")
+    public ServletRegistrationBean createServletRegistrationBean() {
+        return new ServletRegistrationBean(new CXFServlet(), "/services/*");
+    }
 
     @Bean
     public Endpoint endpoint() {
